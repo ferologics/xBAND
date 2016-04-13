4b5a15c15f86554bcb9579d8a52052b2137cee8b
ferologics <f.hetes@gmail.com>
2016-04-13 12:58:52 -0700

    Anal commit

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import React, {
+  AppRegistry,
+} from 'react-native';
+
+AppRegistry.registerComponent('clean_project', () => require('./Navigation'));
